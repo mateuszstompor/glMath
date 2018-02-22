f60fce9827809f834bfa629feeef52f0eca67a29
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-02-22 17:45:55 +0100

    clean up, functions refactor. Changed type of dimension in vector to alias to another file

--- source/vector.h
+++ source/vector.h
@@ -14,11 +14,13 @@
 #include <cmath>
 #include <cstring>
 
+#include "common.h"
+
 namespace ms {
     
 	namespace math {
 		
-		template <typename Type, std::uint16_t Dimension>
+		template <typename Type, UNSIGNED_TYPE Dimension>
 		class Vector {
 		
 		public:
@@ -42,8 +44,8 @@ namespace ms {
 			Vector 			operator 	* 		(float value) const;
 			Vector& 		operator 	*= 		(float value);
 			
-			Type & 			operator 	[] 		(std::uint16_t position);
-			Type & 			at					(std::uint16_t position);
+			Type & 			operator 	[] 		(UNSIGNED_TYPE position);
+			Type & 			at					(UNSIGNED_TYPE position);
 			float 			dot					(const Vector & v) const;
 			Vector 			cross				(const Vector & v) const;
 			
@@ -65,39 +67,36 @@ namespace ms {
     
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::Vector() : components( new Type[Dimension] ) { }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::Vector(Type value) : Vector() {
-    
-    for (std::uint16_t i = 0; i < Dimension; ++i)
-        components[i] = value;
-    
+	std::fill_n((*this).components, Dimension, value);
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::Vector(Vector && v) : components(v.components) {
     v.components = nullptr;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::Vector(const Vector & v) : components( new Type[Dimension] ) {
-    memcpy((*this).components, v.components, Dimension * sizeof(Type));
+    std::memcpy((*this).components, v.components, Dimension * sizeof(Type));
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::Vector(const Type array [Dimension]) : components( new Type[Dimension] ) {
-	memcpy((*this).components, array, Dimension * sizeof(Type));
+	std::memcpy((*this).components, array, Dimension * sizeof(Type));
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>& ms::math::Vector<Type, Dimension>::operator=(const Vector & v) {
-    memcpy((*this).components, v.components, Dimension * sizeof(Type));
+    std::memcpy((*this).components, v.components, Dimension * sizeof(Type));
     return *this;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>& ms::math::Vector<Type, Dimension>::operator=(Vector && v) {
     delete [] (*this).components;
     
@@ -107,35 +106,35 @@ ms::math::Vector<Type, Dimension>& ms::math::Vector<Type, Dimension>::operator=(
     return *this;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension>::~Vector() {
     delete [] this->components;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 bool ms::math::Vector<Type, Dimension>::operator == (const Vector & v) const {
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         if (v.components[i] != (*this).components[i])
             return false;
     return true;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 bool ms::math::Vector<Type, Dimension>::operator != (const Vector & v) const {
     return !( *this == v );
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 float ms::math::Vector<Type, Dimension>::dot(const Vector & v) const {
     float dotProduct = 0.0f;
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         dotProduct += (*this).components[i] * v.components[i];
     
     return dotProduct;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> ms::math::Vector<Type, Dimension>::cross(const Vector & v) const {
 	static_assert(Dimension == 3, "Cross product exists only for dimension of three");
 	Vector vec;
@@ -145,112 +144,112 @@ ms::math::Vector<Type, Dimension> ms::math::Vector<Type, Dimension>::cross(const
 	return vec;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> & ms::math::Vector<Type, Dimension>::operator *= (float value) {
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         (*this).components[i] *= value;
     
     return *this;
     
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> ms::math::Vector<Type, Dimension>::operator * (float value) const {
     
     Vector vec;
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         vec.components[i] = (*this).components[i] * value;
     
     return vec;
     
 }
 
-template <typename Type, std::uint16_t Dimension>
-Type& ms::math::Vector<Type, Dimension>::at(std::uint16_t position) {
+template <typename Type, UNSIGNED_TYPE Dimension>
+Type& ms::math::Vector<Type, Dimension>::at(UNSIGNED_TYPE position) {
     return (*this).components[position];
 }
 
-template <typename Type, std::uint16_t Dimension>
-Type& ms::math::Vector<Type, Dimension>::operator [] (std::uint16_t position) {
+template <typename Type, UNSIGNED_TYPE Dimension>
+Type& ms::math::Vector<Type, Dimension>::operator [] (UNSIGNED_TYPE position) {
     return (*this).components[position];
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> operator * (float value, const ms::math::Vector<Type, Dimension> & v) {
     ms::math::Vector<Type, Dimension> vec(v);
     return vec * value;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> ms::math::Vector<Type, Dimension>::operator + (const Vector & v) const {
     Vector vec;
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         vec.components[i] = (*this).components[i] + v.components[i];
     
     return vec;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> ms::math::Vector<Type, Dimension>::operator - (const Vector & v) const {
     Vector vec;
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         vec.components[i] = (*this).components[i] - v.components[i];
     
     return vec;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> & ms::math::Vector<Type, Dimension>::operator += (const Vector & v) {
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         (*this).components[i] += v.components[i];
     
     return *this;
     
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 ms::math::Vector<Type, Dimension> & ms::math::Vector<Type, Dimension>::operator -= (const Vector & v) {
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         (*this).components[i] -= v.components[i];
     
     return *this;
     
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 float ms::math::Vector<Type, Dimension>::length() const {
     float length = 0.0;
     
-    for(std::uint16_t i = 0; i < Dimension; ++i)
+    for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         length += (*this).components[i] * (*this).components[i];
     
     return std::sqrt(length);
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 void ms::math::Vector<Type, Dimension>::normalize() {
 	float len = length();
-	for(std::uint16_t i = 0; i < Dimension; ++i)
+	for(UNSIGNED_TYPE i = 0; i < Dimension; ++i)
 		components[i] /= len;
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 std::string ms::math::Vector<Type, Dimension>::to_string() const {
     std::ostringstream output;
     
-    for (std::uint16_t i = 0; i < Dimension; ++i)
+    for (UNSIGNED_TYPE i = 0; i < Dimension; ++i)
         output << components[i] << " ";
     
     return output.str();
 }
 
-template <typename Type, std::uint16_t Dimension>
+template <typename Type, UNSIGNED_TYPE Dimension>
 const Type * ms::math::Vector<Type, Dimension>::c_array() const {
     return components;
 }
