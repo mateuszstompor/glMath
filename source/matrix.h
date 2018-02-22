99007b9e2f2ae6ac5dc8b3d1819d0305c4b23503
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-02-22 17:38:53 +0100

    implemented constructors and destructor

--- source/matrix.h
+++ source/matrix.h
@@ -10,6 +10,7 @@
 #define matrix_h
 
 #include <string>
+#include <cstring>
 
 #include "common.h"
 
@@ -32,37 +33,33 @@ namespace ms {
 			
 							Matrix				(const Type array [Rows * Columns]);
 			
+							~Matrix				();
+			
 			Matrix &		operator =			(const Matrix & m);
 		
 			Matrix &		operator =			(Matrix && m);
 			
-			Matrix 			operator - 			(const Matrix & m);
+			Matrix 			operator - 			(const Matrix & m) const;
 			
 			Matrix & 		operator -= 		(const Matrix & m);
 			
-			Matrix 			operator + 			(const Matrix & m);
+			Matrix 			operator + 			(const Matrix & m) const;
 			
 			Matrix & 		operator += 		(const Matrix & m);
 			
 			Matrix & 		operator *= 		(const Matrix & m);
 			
-			Matrix	 		operator *	 		(const Matrix & m);
+			Matrix	 		operator *	 		(const Matrix & m) const;
 			
 			Matrix & 		operator *= 		(Type value);
 			
-			Matrix	 		operator *	 		(Type value);
+			Matrix	 		operator *	 		(Type value) const;
 			
 			bool	 		operator ==	 		(const Matrix & m);
 			
 			bool	 		operator !=	 		(const Matrix & m);
 			
 			Type & 			operator []			(UNSIGNED_TYPE index);
-			
-			Matrix 			add 				(const Matrix & m);
-			
-			Matrix 			subtract 			(const Matrix & m);
-			
-			Type & 			at					(UNSIGNED_TYPE index);
 			
 			std::string 	to_string			() const;
 			
@@ -78,20 +75,35 @@ namespace ms {
 	
 }
 
-//TODO
-//Matrix				();
 
-//TODO
-//Matrix				(Type value);
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+ms::math::Matrix<Type, Rows, Columns> :: Matrix() : components(new Type[ Columns * Rows ]) { }
 
-//TODO
-//Matrix				(const Matrix & m);
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+ms::math::Matrix<Type, Rows, Columns> :: Matrix(Type value) : Matrix() {
+	std::memset((*this).components, value, sizeof(Type) * Rows * Columns);
+}
 
-//TODO
-//Matrix				(Matrix && m);
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
 
-//TODO
-//Matrix				(const Type array [Rows * Columns]);
+ms::math::Matrix<Type, Rows, Columns> :: Matrix(const Matrix & m) : Matrix() {
+	std::memcpy((*this).components, m.components, sizeof(Type) * Rows * Columns);
+}
+
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+ms::math::Matrix<Type, Rows, Columns> :: Matrix(Matrix && m) : components(m.components) {
+	m.components = nullptr;
+}
+
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+ms::math::Matrix<Type, Rows, Columns> :: Matrix(const Type array [Rows * Columns]) : Matrix() {
+	std::memcpy((*this).components, array, sizeof(Type) * Rows * Columns);
+}
+
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+ms::math::Matrix<Type, Rows, Columns> :: ~Matrix() {
+	delete [] (*this).components;
+}
 
 //TODO
 //Matrix&			operator =			(const Matrix & m);
@@ -130,21 +142,14 @@ namespace ms {
 //bool	 		operator !=	 		(const Matrix & m);
 
 //TODO
-//Matrix 			add 				(const Matrix & m);
-
-//TODO
-//Matrix 			subtract 			(const Matrix & m);
-
-//TODO
 //Type & 			operator []			(UNSIGNED_TYPE index);
-
-//TODO
-//Type & 			at					(UNSIGNED_TYPE index);
 
 //TODO
 //std::string 	to_string			() const;
 
-//TODO
-//const Type * 	c_array				() const;
+template <typename Type, UNSIGNED_TYPE Rows, UNSIGNED_TYPE Columns>
+const Type * ms::math::Matrix<Type, Rows, Columns> :: c_array() const {
+	return (*this).components;
+}
 
 #endif
