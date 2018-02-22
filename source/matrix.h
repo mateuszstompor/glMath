6a1f5a0bf63a1e67caf267566bedc0d6b6fe6ba5
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-02-22 17:19:12 +0100

    added interface for matrix class

--- source/matrix.h
+++ source/matrix.h
@@ -9,6 +9,8 @@
 #ifndef matrix_h
 #define matrix_h
 
+#include <string>
+
 #include "common.h"
 
 namespace ms {
@@ -22,7 +24,45 @@ namespace ms {
 			
 							Matrix				();
 			
+							Matrix				(Type value);
+			
+							Matrix				(const Matrix & m);
+			
+							Matrix				(Matrix && m);
+			
 							Matrix				(const Type array [Rows * Columns]);
+			
+			Matrix &		operator =			(const Matrix & m);
+		
+			Matrix &		operator =			(Matrix && m);
+			
+			Matrix 			operator - 			(const Matrix & m);
+			
+			Matrix & 		operator -= 		(const Matrix & m);
+			
+			Matrix 			operator + 			(const Matrix & m);
+			
+			Matrix & 		operator += 		(const Matrix & m);
+			
+			Matrix & 		operator *= 		(const Matrix & m);
+			
+			Matrix	 		operator *	 		(const Matrix & m);
+			
+			Matrix & 		operator *= 		(Type value);
+			
+			Matrix	 		operator *	 		(Type value);
+			
+			bool	 		operator ==	 		(const Matrix & m);
+			
+			bool	 		operator !=	 		(const Matrix & m);
+			
+			Type & 			operator []			(UNSIGNED_TYPE index);
+			
+			Matrix 			add 				(const Matrix & m);
+			
+			Matrix 			subtract 			(const Matrix & m);
+			
+			Type & 			at					(UNSIGNED_TYPE index);
 			
 			std::string 	to_string			() const;
 			
@@ -37,5 +77,74 @@ namespace ms {
 	}
 	
 }
+
+//TODO
+//Matrix				();
+
+//TODO
+//Matrix				(Type value);
+
+//TODO
+//Matrix				(const Matrix & m);
+
+//TODO
+//Matrix				(Matrix && m);
+
+//TODO
+//Matrix				(const Type array [Rows * Columns]);
+
+//TODO
+//Matrix&			operator =			(const Matrix & m);
+
+//TODO
+//Matrix&			operator =			(Matrix && m);
+
+//TODO
+//Matrix 			operator - 			(const Matrix & m);
+
+//TODO
+//Matrix & 			operator -= 		(const Matrix & m);
+
+//TODO
+//Matrix 			operator + 			(const Matrix & m);
+
+//TODO
+//Matrix & 			operator += 		(const Matrix & m);
+
+//TODO
+//Matrix & 		operator *= 		(const Matrix & m);
+
+//TODO
+//Matrix	 		operator *	 		(const Matrix & m);
+
+//TODO
+//Matrix & 		operator *= 		(Type value);
+
+//TODO
+//Matrix	 		operator *	 		(Type value);
+
+//TODO
+//bool	 		operator ==	 		(const Matrix & m);
+
+//TODO
+//bool	 		operator !=	 		(const Matrix & m);
+
+//TODO
+//Matrix 			add 				(const Matrix & m);
+
+//TODO
+//Matrix 			subtract 			(const Matrix & m);
+
+//TODO
+//Type & 			operator []			(UNSIGNED_TYPE index);
+
+//TODO
+//Type & 			at					(UNSIGNED_TYPE index);
+
+//TODO
+//std::string 	to_string			() const;
+
+//TODO
+//const Type * 	c_array				() const;
 
 #endif
