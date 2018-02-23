d6db293a7d6e121085fbc80336fe239a4fda86b2
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-02-23 14:42:41 +0100

    added part of implementation of functions

--- tests/matrix/matrix_tests.cpp
+++ tests/matrix/matrix_tests.cpp
@@ -11,3 +11,40 @@ void MatrixTest::setUp() {
 void MatrixTest::tearDown() {
 
 }
+
+void MatrixTest::testConstructors() {
+	
+	ms::math::Matrix<float, 3, 3> m(1);
+	for(int i = 0; i < 9; ++i)
+		CPPUNIT_ASSERT(m[i] == 1.0f);
+	
+}
+
+void MatrixTest::testAddition() {
+	ms::math::Matrix<float, 3, 3> m1(1);
+	ms::math::Matrix<float, 3, 3> m2(2);
+	ms::math::Matrix<float, 3, 3> result;
+	result = m1 + m2;
+	for(int i = 0; i < 9; ++i)
+		CPPUNIT_ASSERT(result[i] == 3.0f);
+}
+
+void MatrixTest::testSubtraction() {
+	ms::math::Matrix<float, 3, 3> m1(1);
+	ms::math::Matrix<float, 3, 3> m2(2);
+	ms::math::Matrix<float, 3, 3> result;
+	result = m1 - m2;
+	for(int i = 0; i < 9; ++i)
+		CPPUNIT_ASSERT(result[i] == -1.0f);
+}
+
+void MatrixTest::testEquality() {
+	
+	ms::math::Matrix<float, 3, 3> m1(1);
+	ms::math::Matrix<float, 3, 3> m2(2);
+	CPPUNIT_ASSERT(m1 != m2);
+	m1 = m2;
+	CPPUNIT_ASSERT(m1 == m2);
+	m1[8] = 3.0f;
+	CPPUNIT_ASSERT(m1 != m2);
+}
--- tests/matrix/matrix_tests.hpp
+++ tests/matrix/matrix_tests.hpp
@@ -8,15 +8,24 @@
 namespace ms {
     
   class MatrixTest : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE( MatrixTest );
+    	CPPUNIT_TEST_SUITE( MatrixTest );
+	
+	  CPPUNIT_TEST( testConstructors );
+	  CPPUNIT_TEST( testAddition );
+	  CPPUNIT_TEST( testSubtraction );
+	  CPPUNIT_TEST( testEquality );
 
-    CPPUNIT_TEST_SUITE_END();
+	  CPPUNIT_TEST_SUITE_END();
 
   public:
       
-    void setUp();
-    void tearDown();
-      
+    	void setUp();
+    	void tearDown();
+	  
+	  void testConstructors();
+	  void testAddition();
+	  void testSubtraction();
+	  void testEquality();
   };
     
 }
