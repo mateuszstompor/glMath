29825a153a76db3c7400804244183b0612f9c06f
Mateusz Stompor <mateusz.stompor@gmail.com>
2018-03-22 16:16:25 +0100

    improved windows support

--- source/internal/common.h
+++ source/internal/common.h
@@ -10,16 +10,25 @@
 #define common_h
 
 #include <cstdint>
-#include <cmath>
 
-// CONSTANS
+#ifdef __WIN32__
+
+    #define _USE_MATH_DEFINES
+    #include <math.h>
+
+#else
+
+    #include <cmath>
+
+#endif
+
 
 namespace ms {
-	
+
 	namespace math {
-		
+
 		#define UNSIGNED_TYPE std::uint8_t
-	
+
 	}
 
 }
@@ -27,66 +36,66 @@ namespace ms {
 // FUNCTIONS
 
 namespace ms {
-	
+
 	namespace math {
-		
+
 		// function that converts radians to angle in degrees
 		template <typename Type>
 		inline Type degrees(Type radians) {
 			return radians * 180.0f / M_PI;
 		}
-		
+
 		template <>
 		inline double degrees(double radians) {
 			return radians * 180.0 / M_PI;
 		}
-		
+
 		template <typename Type>
 		inline Type radians(Type degrees) {
 			return M_PI * (degrees / 180.0f);
 		}
-		
+
 		template <>
 		inline double radians(double degrees) {
 			return M_PI * (degrees / 180.0);
 		}
-		
-		
+
+
 		// cosine
 		template <typename Type>
 		inline Type cosine(Type radians) {
 			return cos(radians);
 		}
-		
+
 		template <>
 		inline float cosine(float radians) {
 			return cosf(radians);
 		}
-		
+
 		// sinus
 		template <typename Type>
 		inline Type sinus(Type radians) {
 			return sin(radians);
 		}
-		
+
 		template <>
 		inline float sinus(float radians) {
 			return sinf(radians);
 		}
-		
+
 		// sqrt
 		template <typename Type>
 		inline Type square_root(Type number) {
 			return sqrt(static_cast<double>(number));
 		}
-		
+
 		template <>
 		inline float square_root(float number) {
 			return sqrt(number);
 		}
-		
+
 	}
-	
+
 }
 
 
