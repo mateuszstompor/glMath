a10315a148a84588120e777c14026614c1f7ca4a
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-04-12 19:45:25 +0200

    add orthographic projection

--- source/internal/projections.h
+++ source/internal/projections.h
@@ -55,6 +55,40 @@ namespace ms {
 				return p;
 			}
 			
+			template <typename Type>
+			Matrix<Type, 4, 4> orthogonal(Type far, Type near,
+										  Type top, Type bottom,
+										  Type left, Type right) {
+				
+				Matrix<Type, 4, 4> p;
+				
+				// First column
+				p[0] 	= Type(2.0)/(right - left);
+				p[1] 	= Type(0.0);
+				p[2] 	= Type(0.0);
+				p[3] 	= Type(0.0);
+				
+				// Second column
+				p[4] 	= Type(0.0);
+				p[5] 	= Type(2.0)/(top - bottom);
+				p[6] 	= Type(0.0);
+				p[7]	= Type(0.0);
+				
+				// Third column
+				p[8] 	= Type(0.0);
+				p[9] 	= Type(0.0);
+				p[10]	= -Type(2.0)/(far - near);
+				p[11] 	= Type(0.0);
+				
+				// Fourth column
+				p[12] 	= -((right + left)/(right - left));
+				p[13] 	= -((top + bottom)/(top - bottom));
+				p[14]	= -((far + near)/(far - near));
+				p[15] 	= Type(1.0);
+				
+				return p;
+			}
+			
 		}
 		
 	}
